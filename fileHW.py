c29d4f3ef135637e27237bb1aca33ee25c5c7cc2
J <247madman@gmail.com>
2017-12-01 20:54:13 -0500

    Update fileHW.py

--- fileHW.py
+++ fileHW.py
@@ -10,7 +10,7 @@ print("Hi "+ name01 +" my name is Hal.\n")
 print("\n How are you feeling today?\n" "")
 feel01 = input("Good or Bad: \n" "")
 if feel01 == 'Good' :
-        print("I'm glade to here that!\n Oh\' I\'m sorry I\'ve got to go computer stuff to do.\n It was nice meeting you...\nBye!")
+        print("I'm glade to hear that!\n Oh\' I\'m sorry I\'ve got to go computer stuff to do.\n It was nice meeting you...\nBye!")
 elif feel01 == 'Bad' :
         input("That\'s not good. Do you want to talk about it?\n" "yes - no:\n " "")
 
